f4b84b0d2897b86046fcefd8f73edd87e1763b11
Andrew Bilych <37004245+AndriiBilych@users.noreply.github.com>
2021-02-12 16:28:21 +0100

    [Engine] added -, -= operator overloads to class Position [Tetris] prototyping

--- Tetris/Tetris.cpp
+++ Tetris/Tetris.cpp
@@ -2,30 +2,73 @@
 
 Tetris::Tetris(int width, int height, int fontWidth, int fontHeight)
 	: Engine(width, height, fontWidth, fontHeight) {
+	currentShape.index = 6;
+	currentShape.statesAmount = one;
+	
+	currentShape.p = Position(1, 1);
+
+	timePassed = .0f;
+	timer = 1.0f;
+	
 	ChangeTitle(L"Tetris");
 }
 
 bool Tetris::Start() {
+	std::copy(&(figures[currentShape.index][0]), &(figures[currentShape.index][4]), currentShape.coords);
 
 	return true;
 }
 
 bool Tetris::Update(float timeElapsed) {
-	DrawShape(0, Position(1, 1));
-	DrawShape(1, Position(5, 1));
-	DrawShape(2, Position(8, 1));
-	DrawShape(3, Position(1, 4));
-	DrawShape(4, Position(1, 8));
-	DrawShape(5, Position(1, 12));
-	DrawShape(6, Position(1, 16));
+
+	ClearScreen();
+
+	DrawShape();
+
+	if (GetKey(VK_UP).pressed) {
+		currentShape.index = currentShape.index == 5 ? 6 : 5;
+		std::copy(&(figures[currentShape.index][0]), &(figures[currentShape.index][4]), currentShape.coords);
+	}
+	//faster descend
+	/*else if (GetKey(VK_DOWN).pressed) {
+		currentShape.index = currentShape.index == 5 ? 6 : 5;
+		std::copy(&(figures[currentShape.index][0]), &(figures[currentShape.index][4]), currentShape.coords);
+	}*/
+
+	if (GetKey(VK_RIGHT).pressed) 
+		currentShape.p += Position(1, 0);
+	else if (GetKey(VK_LEFT).pressed) 
+		currentShape.p -= Position(1, 0);
+	
+	//Timer for descending
+	timePassed += timeElapsed;
+	if (timePassed >= timer)
+	{
+		if (currentShape.p.y < GetScreenHeight() - 1)
+			currentShape.p += Position(0, 1);
+
+		timePassed -= timer;
+	}
 
 	return true;
 }
 
-void Tetris::DrawShape(int index, Position p) {
+void Tetris::DrawShape() {
 	for (int i = 0; i < 4; i++) {
-		Draw(p + figures[index][i], ' ', BG_GREY);
+		Draw(currentShape.p + currentShape.coords[i], ' ', BG_GREY);
 	}
+}
+
+void Tetris::RandomShape() {
+
+}
+
+int Tetris::NextRotation(int) {
+
+}
+
+Position Tetris::GetLowestPoint(Position rotations[4]) {
+
 }
 
 Tetris::~Tetris() {
--- Tetris/Tetris.h
+++ Tetris/Tetris.h
@@ -3,23 +3,62 @@
 #include <algorithm>
 #include "Engine.h"
 
+enum StatesAmount {
+    one,
+    two,
+    four
+};
+
+struct Shape {
+public:
+    Position coords[4];
+    int index;
+    StatesAmount statesAmount;
+    Position p;
+};
+
 class Tetris : public Engine
 {
 public:
     Tetris(int, int, int, int);
     bool Start();
     bool Update(float);
-    void DrawShape(int, Position);
+    void DrawShape();
+    void RandomShape();
+    int NextRotation(int);
+    Position GetLowestPoint(Position[4]);
     ~Tetris();
 private:
-    Position figures[7][4] = {
-        { Position(0, 0), Position(1, 0), Position(0, 1), Position(1, 1) }, //square boi
-        { Position(0, 0), Position(0, 1), Position(0, 2), Position(0, 3) }, //long boi
-        { Position(1, 0), Position(2, 0), Position(0, 1), Position(1, 1) }, //s boi
-        { Position(0, 0), Position(1, 0), Position(1, 1), Position(2, 1) }, //z boi
-        { Position(0, 0), Position(0, 1), Position(0, 2), Position(1, 2) }, //L boi
-        { Position(1, 0), Position(1, 1), Position(1, 2), Position(0, 2) }, //J boi
-        { Position(0, 0), Position(1, 0), Position(2, 0), Position(1, 1) } //t boi
+    float timer;
+    float timePassed;
+    Shape currentShape;
+    Position figures[19][4] = {
+        { Position(0, 0), Position(1, 0), Position(0, 1), Position(1, 1) }, //0 square boi
+
+        { Position(0, 0), Position(0, 1), Position(0, 2), Position(0, 3) }, //1 long boi - vertical
+        { Position(0, 2), Position(1, 2), Position(2, 2), Position(3, 2) }, //2 long boi - horizontal
+
+        { Position(0, 0), Position(0, 1), Position(1, 1), Position(1, 2) }, //3 s boi - vertical
+        { Position(1, 0), Position(2, 0), Position(0, 1), Position(1, 1) }, //4 s boi - horizontal
+
+        { Position(1, 0), Position(1, 1), Position(0, 1), Position(0, 2) }, //5 z boi - vertical
+        { Position(0, 0), Position(1, 0), Position(1, 1), Position(2, 1) }, //6 z boi - horizontal
+
+        //-----------------------------------------------------------------------------------
+        { Position(0, 0), Position(0, 1), Position(0, 2), Position(1, 2) }, //7 L boi - north
+        { Position(0, 0), Position(0, 1), Position(0, 2), Position(1, 2) }, //8 L boi - east
+        { Position(0, 0), Position(0, 1), Position(0, 2), Position(1, 2) }, //9 L boi - south
+        { Position(0, 0), Position(0, 1), Position(0, 2), Position(1, 2) }, //10 L boi - west
+
+        { Position(1, 0), Position(1, 1), Position(1, 2), Position(0, 2) }, //11 J boi - north
+        { Position(1, 0), Position(1, 1), Position(1, 2), Position(0, 2) }, //12 J boi - east
+        { Position(1, 0), Position(1, 1), Position(1, 2), Position(0, 2) }, //13 J boi - south
+        { Position(1, 0), Position(1, 1), Position(1, 2), Position(0, 2) }, //14 J boi - west
+
+        { Position(0, 0), Position(1, 0), Position(2, 0), Position(1, 1) }, //15 t boi - north
+        { Position(0, 0), Position(1, 0), Position(2, 0), Position(1, 1) }, //16 t boi - east
+        { Position(0, 0), Position(1, 0), Position(2, 0), Position(1, 1) }, //17 t boi - south
+        { Position(0, 0), Position(1, 0), Position(2, 0), Position(1, 1) } //18 t boi - west
     };
 };
 
